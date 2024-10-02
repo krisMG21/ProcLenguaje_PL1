1a6833519e052de9e0ab41db9c09f0d4658c2bcd
krisMG21 <cristimg9@gmail.com>
2024-10-02 23:56:40 +0200

    Update tests.py
    using pytest

--- src/tests.py
+++ src/tests.py
@@ -1,13 +1,12 @@
 import json
-
+import pytest
 from maquina import Alfabeto, Estados, Matriz, Maquina
 
-config = json.load(open('test.json'))
+@pytest.fixture
+def config():
+    return json.load(open('test.json'))
 
-def alfabeto_test():
-    '''Alfabeto unit testing'''
-
-    alfabeto = Alfabeto('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+def test_alfabeto():
     alfabeto = Alfabeto('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     assert alfabeto[25] == 'Z'
     assert alfabeto.index('A') == 0
@@ -22,9 +21,7 @@ def alfabeto_test():
     assert len(alfabeto) == 26
     assert list(alfabeto) == list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-def estados_test():
-    '''Estados unit testing'''
-
+def test_estados():
     estados = Estados([0, 1, 2], 0, [2])
     assert estados.get_inicial() == 0
     assert estados.is_estado(1)
@@ -32,76 +29,63 @@ def estados_test():
     assert estados.is_final(2)
     assert not estados.is_final(1)
 
-def matriz_test():
-    '''Matriz unit testing'''
+def test_matriz(config):
     matriz = Matriz(config["alfabeto"], config["matriz"])
 
-    assert matriz["0"] == {"a" : 1}
-    assert matriz["1"] == {"a" : 1, "b" : 2}
-    assert matriz["2"] == {"b" : 2, "c" : 3}
+    assert matriz["0"] == {"a": 1}
+    assert matriz["1"] == {"a": 1, "b": 2}
+    assert matriz["2"] == {"b": 2, "c": 3}
     assert matriz["3"] == {}
 
-    assert str(matriz) == \
-        '  a b c \n' + \
-        '0 1 _ _ \n' + \
-        '1 1 2 _ \n' + \
-        '2 _ 2 3 \n' + \
+    expected_str = (
+        '  a b c \n'
+        '0 1 _ _ \n'
+        '1 1 2 _ \n'
+        '2 _ 2 3 \n'
         '3 _ _ _ \n'
+    )
+    assert str(matriz) == expected_str
 
-def maquina_test():
-    '''Maquina unit testing'''
-
+def test_maquina():
     maquina = Maquina('test.json')
     assert maquina.parse('abc')
     assert maquina.parse('aabbc')
     assert not maquina.parse('abca')
     assert not maquina.parse('abcabc')
 
-    assert maquina.trace('abc') == \
-        [(0, 'a', 1),
-         (1, 'b', 2),
-         (2, 'c', 3)]
-    assert maquina.trace('aabbc') == \
-        [(0, 'a', 1),
-         (1, 'a', 1),
-         (1, 'b', 2),
-         (2, 'b', 2),
-         (2, 'c', 3)]
-    assert maquina.trace('abca') == \
-        [(0, 'a', 1),
-         (1, 'b', 2),
-         (2, 'c', 3),
-         (3, 'a', -1)]
-    assert maquina.trace('error') == \
-        [(0, 'e', -1),
+    assert maquina.trace('abc') == [
+        (0, 'a', 1),
+        (1, 'b', 2),
+        (2, 'c', 3)
+    ]
+    assert maquina.trace('aabbc') == [
+        (0, 'a', 1),
+        (1, 'a', 1),
+        (1, 'b', 2),
+        (2, 'b', 2),
+        (2, 'c', 3)
+    ]
+    assert maquina.trace('abca') == [
+        (0, 'a', 1),
+        (1, 'b', 2),
+        (2, 'c', 3),
+        (3, 'a', -1)
+    ]
+    assert maquina.trace('error') == [
+        (0, 'e', -1),
         (-1, 'r', -1),
         (-1, 'r', -1),
         (-1, 'o', -1),
-        (-1, 'r', -1)]
+        (-1, 'r', -1)
+    ]
 
     # Para cualquier caso, no hay mas de n palabras, con no mas de m caracteres
     n = 100
     m = 10
 
     # Comprobamos que todas las cadenas son válidas y no exceden ni n ni m
-    for i, cadena in enumerate(maquina.generate(n, m)):
+    generated = list(maquina.generate(n, m))
+    assert len(generated) <= n
+    for cadena in generated:
         assert len(cadena) <= m
         assert maquina.parse(cadena)
-    assert i <= n
-
-
-def main():
-    '''Main function'''
-    try:
-        alfabeto_test()
-        estados_test()
-        matriz_test()
-        maquina_test()
-    except AssertionError as e:
-        raise e
-    else:
-        print("Tests passed")
-        exit(0)
-
-if __name__ == '__main__':
-    main()
