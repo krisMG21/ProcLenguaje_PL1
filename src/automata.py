c44d59ffea4ced7f704cc57eec6bf45cec8928ed
krisMG21 <cristimg9@gmail.com>
2024-10-08 12:46:25 +0200

    Update automata.py

--- src/automata.py
+++ src/automata.py
@@ -4,8 +4,9 @@ import json
 
 # ESTADOS ===================================================================
 
+
 class Estados:
-    '''
+    """
     private estados: list[int]
     private inicial: int
     private finales: list[int]
@@ -15,31 +16,37 @@ class Estados:
 
     Encapsula los estados y provee de las funciones
     y accesos esenciales.
-    '''
-    
-    def __init__(self, estados : list[int], inicial : int, finales : list[int]):
+    """
+
+    def __init__(self, estados: list[int], inicial: int, finales: list[int]):
         self.estados = estados
         self.inicial = inicial
         self.finales = finales
 
     def __str__(self):
-        '''Devuelve los estados como una cadena'''
-        return str(self.estados) + \
-            '\n Inicial: ' + str(self.inicial) + \
-            '\n Finales: ' + str(self.finales)
+        """Devuelve los estados como una cadena"""
+        return (
+            str(self.estados)
+            + "\n Inicial: "
+            + str(self.inicial)
+            + "\n Finales: "
+            + str(self.finales)
+        )
 
     def get_inicial(self):
-        '''Devuelve el estado inicial'''
+        """Devuelve el estado inicial"""
         return self.inicial
 
-    def is_final(self, estado : int):
-        '''Devuelve si el estado es final'''
+    def is_final(self, estado: int):
+        """Devuelve si el estado es final"""
         return estado in self.finales
+
 
 # MATRIZ ====================================================================
 
+
 class Matriz:
-    '''
+    """
     private matriz: dict{"estado" : dict{letra : estado}}
 
     Contiene una matriz de estados y letras, implementada
@@ -47,7 +54,7 @@ class Matriz:
 
     Encapsula la matriz y provee de las funciones
     y accesos esenciales.
-    '''
+    """
 
     def __init__(self, matriz: dict):
         self.matriz = matriz
@@ -59,67 +66,67 @@ class Matriz:
         for estado in self.matriz.values():
             alfabeto.update(letra for letra in estado.keys())
 
-        self.alfabeto = str.join('', sorted(list(alfabeto)))
+        self.alfabeto = str.join("", sorted(list(alfabeto)))
 
     def __getitem__(self, estado):
-        '''Para acceder de la forma matriz[estado][letra]'''
+        """Para acceder de la forma matriz[estado][letra]"""
         return self.matriz[estado]
 
     def __str__(self):
-        '''Devuelve la matriz como una cadena tipo:
-              a b c ... z
-            0 1 3 _ ... _
-            1 2 4 4 ... _
-            2 _ 3 5 ... _
-            . . . . ... .
-            9 _ _ _ ... _
-        '''
+        """Devuelve la matriz como una cadena tipo:
+          a b c ... z
+        0 1 3 _ ... _
+        1 2 4 4 ... _
+        2 _ 3 5 ... _
+        . . . . ... .
+        9 _ _ _ ... _
+        """
         # Fila de letras
-        cadena = '  ' + ' '.join(self.alfabeto) + ' \n'
+        cadena = "  " + " ".join(self.alfabeto) + " \n"
 
         # Recorremos la matriz
         for estado in self.estados:
-            cadena += str(estado) + ' '
+            cadena += str(estado) + " "
             for letra in self.alfabeto:
                 # Si no existe esa transición, se añade un '_' en su lugar
                 try:
-                    cadena += str(self.matriz[str(estado)][letra]) + ' '
+                    cadena += str(self.matriz[str(estado)][letra]) + " "
                 except KeyError:
-                    cadena += '_ '
-            cadena += '\n'
+                    cadena += "_ "
+            cadena += "\n"
 
         return cadena
 
-class Automata:
-    ''' Representa un autómata de una expresión regular '''
 
-    def __init__(self, expr_json : str):
-        '''Carga los componentes de la maquina desde un json'''
+class Automata:
+    """Representa un autómata de una expresión regular"""
+
+    def __init__(self, expr_json: str):
+        """Carga los componentes de la maquina desde un json"""
         config = json.load(open(expr_json))
-        self.matriz = Matriz(config['matriz'])
+        self.matriz = Matriz(config["matriz"])
         self.estados = Estados(
-            self.matriz.estados,
-            config['inicial'],
-            config['finales'])
-        self.expr = config['expresion']
-        self.state = config['inicial']
+            self.matriz.estados, config["inicial"], config["finales"]
+        )
+        self.expr = config["expresion"]
+        self.state = config["inicial"]
 
     def get_alfabeto(self):
-        '''Devuelve el alfabeto'''
+        """Devuelve el alfabeto"""
         return self.matriz.alfabeto
 
     def get_matriz(self):
-        '''Devuelve la matriz'''
+        """Devuelve la matriz"""
         return self.matriz
 
     def get_estados(self):
-        '''Devuelve los estados de forma legible'''
+        """Devuelve los estados de forma legible"""
         return self.estados
 
     def get_expr(self):
-        '''Devuelve la expresión'''
+        """Devuelve la expresión"""
         return self.expr
 
     def get_state(self):
-        '''Devuelve el estado actual'''
+        """Devuelve el estado actual"""
         return self.state
